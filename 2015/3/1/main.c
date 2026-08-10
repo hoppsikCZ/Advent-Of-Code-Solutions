811872a0ef46e0ccb93e23133dbaa1f98a60fb35
hoppsik <tobias.hopp@email.cz>
2026-08-10 15:40:05 +0100

    add 2015 day 3, day4-1

---
+++ 2015/3/1/main.c
@@ -0,0 +1,78 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Vector2D {
+    int x;
+    int y;
+};
+
+struct House {
+    struct Vector2D location;
+    int gift_count;
+};
+
+bool vecotor2d_equals(struct Vector2D *a, struct Vector2D *b) {
+    // printf("comparing %d:%d and %d:%d\n", a->x, a->y, b->x, b->y);
+    if (a->x == b->x && a->y == b->y)
+        return true;
+
+    return false;
+}
+
+int house_exists(struct House *house, size_t size, struct Vector2D *position) {
+    for (size_t i = 0; i < size; i++) {
+        if (vecotor2d_equals(&house[i].location, position))
+            return i;
+    }
+
+    return -1;
+}
+
+int main(int argc, char *argv[]) {
+    FILE *fr = fopen("../input.txt", "r");
+    if (fr == NULL) {
+        printf("Failed to open file!");
+        return -1;
+    }
+
+    int c;
+    struct House *house_arr;
+    size_t house_arr_size = 1;
+    struct Vector2D pos = {0, 0};
+
+    house_arr = malloc(sizeof(struct House));
+    house_arr[0] = (struct House){(struct Vector2D){0, 0}, 1};
+    int test = 0;
+    while ((c = fgetc(fr)) != EOF) {
+        if (c == '>')
+            pos.x++;
+        else if (c == '<')
+            pos.x--;
+        else if (c == '^')
+            pos.y--;
+        else if (c == 'v')
+            pos.y++;
+        else {
+            continue;
+        }
+        // printf("Position is %d:%d\n", pos.x, pos.y);
+
+        int i;
+        if ((i = house_exists(house_arr, house_arr_size, &pos)) >= 0) {
+            house_arr[i].gift_count++;
+        } else {
+            // printf("Adding house number %zu on %d:%d\n", house_arr_size,
+            // pos.x,
+            // pos.y);
+            house_arr_size++;
+            house_arr =
+                realloc(house_arr, sizeof(struct House) * house_arr_size);
+            house_arr[house_arr_size - 1] = (struct House){pos, 1};
+        }
+    }
+
+    printf("%zu\n", house_arr_size);
+    return EXIT_SUCCESS;
+}
---
+++ 2015/4/1/main.c
@@ -0,0 +1,70 @@
+#include <openssl/evp.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// https://docs.openssl.org/master/man3/EVP_DigestInit/#examples
+unsigned int calc_md5_hex(const char *key, unsigned char *md_value) {
+    EVP_MD_CTX *mdctx;
+    unsigned int md_len, i;
+
+    mdctx = EVP_MD_CTX_new();
+    if (mdctx == NULL) {
+        printf("Message digest create failed.\n");
+        exit(1);
+    }
+    if (!EVP_DigestInit_ex2(mdctx, EVP_md5(), NULL)) {
+        printf("Message digest initialization failed.\n");
+        EVP_MD_CTX_free(mdctx);
+        exit(1);
+    }
+    if (!EVP_DigestUpdate(mdctx, key, strlen(key))) {
+        printf("Message digest update failed.\n");
+        EVP_MD_CTX_free(mdctx);
+        exit(1);
+    }
+    if (!EVP_DigestFinal_ex(mdctx, md_value, &md_len)) {
+        printf("Message digest finalization failed.\n");
+        EVP_MD_CTX_free(mdctx);
+        exit(1);
+    }
+    EVP_MD_CTX_free(mdctx);
+
+    printf("Digest is: ");
+    for (i = 0; i < md_len; i++) {
+        printf("%02x", md_value[i]);
+    }
+    printf("\n");
+
+    return md_len;
+}
+
+int main(int argc, char *argv[]) {
+    unsigned char md5[EVP_MAX_MD_SIZE];
+    unsigned int md5_len;
+    const unsigned int to_match[] = {0x00, 0x00};
+    char num[30];
+    const char *key = "bgvyzdsv";
+    char key_num[40];
+
+    for (size_t i = 0; i < SIZE_MAX; i++) {
+        sprintf(num, "%lu", i);
+        strcpy(key_num, key);
+        strcat(key_num, num);
+        md5_len = calc_md5_hex(key_num, md5);
+
+        if (memcmp(to_match, md5, 2) == 0) {
+            if ((int)md5[2] / 10 == 0) {
+                printf("%lu\n", i);
+                break;
+            }
+        }
+    }
+    // printf("Digest is: ");
+    // for (int i = 0; i < md5_len; i++) {
+    //     printf("%02x", md5[i]);
+    // }
+    // printf("\n");
+    return EXIT_SUCCESS;
+}
